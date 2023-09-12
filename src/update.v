9cb36e159cd58527c3756a4fb54e53c87898f861
David Pello <dvpg@tuta.io>
2023-09-12 14:10:03 +0200

    Parse all NoIP return codes

--- src/update.v
+++ src/update.v
@@ -3,11 +3,23 @@ module main
 import net.http
 import encoding.base64
 
+pub enum NoIPCode as u8 {
+	good
+	nochg
+	nohost
+	badauth
+	badagent
+	donator
+	abuse
+	c911
+	unknown
+}
+
 const (
 	user_agent_str = 'Ladecadence.net NoIP/0.1 zako@ladecadence.net'
 )
 
-fn update(host Host) bool {
+fn update(host Host) NoIPCode {
 	// create base64 encoded auth header
 	auth_str := 'Basic ' + base64.encode_str('${host.username}:${host.password}')
 
@@ -18,8 +30,25 @@ fn update(host Host) bool {
 		url: host.update_url + '?hostname=${host.hostname}&offline=${host.offline}'
 		user_agent: user_agent_str
 	}
+	// add headers and send request
 	request.add_header(http.CommonHeader.authorization, auth_str)
-	resp := request.do() or { panic(err) }
+	resp := request.do() or {
+		eprintln('Problem sending HTTP request:')
+		panic(err)
+	}
 
-	return resp.body.contains('good') || resp.body.contains('nochg')
+	// check response
+	code := resp.body.trim_space().split_any(' \n')
+
+	return match code[0] {
+		'good' { NoIPCode.good }
+		'nochg' { NoIPCode.nochg }
+		'nohost' { NoIPCode.nohost }
+		'badauth' { NoIPCode.badauth }
+		'badagent' { NoIPCode.badagent }
+		'!donator' { NoIPCode.donator }
+		'abuse' { NoIPCode.abuse }
+		'911' { NoIPCode.c911 }
+		else { NoIPCode.unknown }
+	}
 }
