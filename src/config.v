636e64c40a78cd6097e1acda77a1dcc531f4d0f1
David Pello <dvpg@tuta.io>
2023-09-12 15:49:27 +0200

    Better error messages and ip option in config file

--- src/config.v
+++ src/config.v
@@ -9,6 +9,7 @@ pub struct Host {
 	username   string
 	password   string
 	hostname   string
+	ip         string
 	offline    string
 }
 
@@ -18,9 +19,15 @@ pub struct Config {
 
 fn get_config(config_file string) Config {
 	// TOML decoder does not work with arrays of tables, so convert it to JSON and decode the JSON
-	doc := toml.parse_file(config_file) or { panic(err) }
+	doc := toml.parse_file(config_file) or {
+		eprintln('Problem reading config file: ')
+		panic(err)
+	}
 	jsondata := to.json(doc)
-	config := json.decode(Config, jsondata) or { panic(err) }
+	config := json.decode(Config, jsondata) or {
+		eprintln('Problem parsing config data: ')
+		panic(err)
+	}
 
 	return config
 }
